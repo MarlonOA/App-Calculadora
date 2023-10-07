bd3ff6bfe504ecdb721bca3af191d583049cdcd3
Marlon <mo24404@gmail.com>
2023-10-07 00:51:26 -0300

    Start Commit!

---
+++ app/src/main/java/com/br/calculadora_simples/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.br.calculadora_simples
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.br.calculadora_simples.databinding.ActivityMainBinding
+import org.mariuszgromada.math.mxparser.Expression
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val calculo = binding.calculo
+
+
+
+        binding.um.setOnClickListener {
+            calculo.text = "${calculo.text}1"
+        }
+
+        binding.dois.setOnClickListener {
+            calculo.text = "${calculo.text}2"
+        }
+
+        binding.tres.setOnClickListener {
+            calculo.text = "${calculo.text}3"
+        }
+
+        binding.quatro.setOnClickListener {
+            calculo.text = "${calculo.text}4"
+        }
+
+        binding.cinco.setOnClickListener {
+            calculo.text = "${calculo.text}5"
+        }
+
+        binding.seis.setOnClickListener {
+            calculo.text = "${calculo.text}6"
+        }
+
+        binding.sete.setOnClickListener {
+            calculo.text = "${calculo.text}7"
+        }
+
+        binding.oito.setOnClickListener {
+            calculo.text = "${calculo.text}8"
+        }
+
+        binding.nove.setOnClickListener {
+            calculo.text = "${calculo.text}9"
+        }
+
+        binding.zero.setOnClickListener {
+            calculo.text = "${calculo.text}0"
+        }
+
+        binding.parenteseAbrindo.setOnClickListener {
+            calculo.text = "${calculo.text}("
+        }
+
+        binding.parenteseFechando.setOnClickListener {
+            calculo.text = "${calculo.text})"
+        }
+
+        binding.divisao.setOnClickListener {
+            calculo.text = "${calculo.text}/"
+        }
+
+        binding.multiplicacao.setOnClickListener {
+            calculo.text = "${calculo.text}*"
+        }
+
+        binding.subtracao.setOnClickListener {
+            calculo.text = "${calculo.text}-"
+        }
+
+        binding.soma.setOnClickListener {
+            calculo.text = "${calculo.text}+"
+        }
+
+        binding.ponto.setOnClickListener {
+            calculo.text = "${calculo.text}."
+        }
+
+        binding.apagar.setOnClickListener {
+            calculo.text = calculo.text.dropLast(1) //dropLast apaga o ultimo caractere
+        }
+
+        binding.CE.setOnClickListener {
+            calculo.text = ""
+            binding.resultado.text = ""
+
+        }
+
+        binding.result.setOnClickListener {
+            val resultadoCalculado = Expression(calculo.text.toString()).calculate()
+
+            if (resultadoCalculado.isNaN()) {
+                binding.resultado.text = "Expressão inválida"
+            }else{
+                binding.result.text = resultadoCalculado.toString()
+            }
+
+
+        }
+    }
+}
